5dc5cb4eaabea1b6dcc862b435d6326961762e93
baptiste <baptiste.name>
2020-05-12 11:19:12 +0200

    Password change done

--- account/views.py
+++ account/views.py
@@ -11,9 +11,9 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd["password"])
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -24,4 +24,4 @@ def user_login(request):
                 return HttpResponse("Invalid login")
     else:
         form = LoginForm()
-        return render(request, "account/login.html", {'form': form})
+        return render(request, "account/login.html", {"form": form})

f26806c7246ac056f20862a5235d4761929dd246
baptiste <baptiste.name>
2020-05-12 14:21:56 +0200

    added possibility to reset passwd by email, deactivate for the moment, need to setup an email

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -19,4 +19,25 @@ urlpatterns = [
         auth_views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
     ),
+    # reset password urls
+    #path(
+    #    "password_reset/", 
+    #    auth_views.PasswordResetView.as_view(), 
+    #    name="password_reset"
+    #),
+    #path(
+    #    "password_reset/done/",
+    #    auth_views.PasswordResetDoneView.as_view(),
+    #    name="password_reset_done",
+    #),
+    #path(
+    #    "reset/<uidb64>/<token>/",
+    #    auth_views.PasswordResetConfirmView.as_view(),
+    #    name="password_reset_confirm",
+    #),
+    #path(
+    #    "reset/done/",
+    #    auth_views.PasswordResetCompleteView.as_view(),
+    #    name="password_reset_complete",
+    #),
 ]

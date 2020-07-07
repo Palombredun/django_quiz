094cc8c72e996f0d768f1220cb964ed3c01e923f
baptiste <baptiste.name>
2020-07-07 14:32:35 +0200

    added authentication with email

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tutorial/"; views.tutorial, name="tutorial")
+    path("tutorial/", views.tutorial, name="tutorial"),
     path("create/", views.create, name="create"),
     path("ajax/load-subcategories", views.load_sub_categories, name="ajax_load_subcategories"),
     

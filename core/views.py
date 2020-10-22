2fff3377347d1c98ba008e8492a40faa613574c5
baptiste <baptiste.name>
2020-10-22 10:02:19 +0200

    Added docstrings in core app

--- core/views.py
+++ core/views.py
@@ -4,15 +4,24 @@ from quiz.models import Quiz
 
 
 def home(request):
+	"""
+	Home page of the website.
+	"""
     quiz_list = Quiz.objects.order_by("-created")[:5]
     return render(request, "core/home.html", {"quiz_list": quiz_list})
 
 
 def handler404(request, exception):
+	"""
+	Custom page 404
+	"""
     data = {}
     return render(request, "core/404.html", data)
 
 
 def handler500(request):
+	"""
+	Custome page 500
+	"""
     data = {}
     return render(request, "core/500.html", data)

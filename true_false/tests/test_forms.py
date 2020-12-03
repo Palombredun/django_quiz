983eda3d9f049730e6b2fc035349df501c89f647
baptiste <baptiste.name>
2020-12-03 11:27:12 +0100

    created test_forms in true_false app

---
+++ true_false/tests/test_forms.py
@@ -0,0 +1,42 @@
+import pytest
+
+from true_false.forms import CreationTrueFalseForm, TrueFalseForm
+
+def test_valid_creation_form():
+	data = {
+		'content': "question",
+		'difficulty': 1,
+		'order': 2,
+		'theme1': "t1",
+		'theme2': 't2',
+		'theme3': 't3',
+		'correct': True,
+	}
+	form = CreationTrueFalseForm(data)
+	assert form.is_valid() is True
+
+def test_invalid_creation_form():
+	data = {
+		'content': "",
+		'difficulty': 1,
+		'order': 2,
+		'theme1': "t1",
+		'theme2': 't2',
+		'theme3': 't3',
+		'correct': True,
+	}
+	form = CreationTrueFalseForm(data)
+	assert form.is_valid() is False
+
+def test_valid_creation_form_without_theme():
+	data = {
+		'content': "question",
+		'difficulty': 1,
+		'order': 2,
+		'theme1': "",
+		'theme2': '',
+		'theme3': '',
+		'correct': True,
+	}
+	form = CreationTrueFalseForm(data)
+	assert form.is_valid() is True

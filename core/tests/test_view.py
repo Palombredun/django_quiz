97bfdee9a121c0a27216e2fe52c5880c75729078
baptiste <baptiste.name>
2020-12-07 17:01:38 +0100

    added docstring to test pages

--- core/tests/test_view.py
+++ core/tests/test_view.py
@@ -2,16 +2,36 @@ import pytest
 from pytest_django.asserts import assertTemplateUsed
 
 def test_home(client, db):
-	response = client.get('/')
+	"""
+	GIVEN a user who wants to visit the home page
+	WHEN he accesses the page
+	THEN assert the right page is sent
+	"""
+	page = "/"
+
+	response = client.get(page)
+
 	assert response.status_code == 200
 	assertTemplateUsed(response, 'core/home.html')
 
 def test_contact(client):
+	"""
+	GIVEN a user who wants to visit the contact page
+	WHEN he access the page
+	THEN assert the right page is sent
+	"""
 	response = client.get('/contact/')
+
 	assert response.status_code == 200
 	assertTemplateUsed(response, 'core/contact.html')
 
 def test_license(client):
+	"""
+	GIVEN a user who wants to visit the license page
+	WHEN he access the page
+	THEN assert the right page is sent
+	"""
 	response = client.get('/license/')
+
 	assert response.status_code == 200
 	assertTemplateUsed(response, 'core/license.html')

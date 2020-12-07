82332f09470dbce044ea3f3435999d46f8343eee
baptiste <baptiste.name>
2020-12-07 16:54:14 +0100

    Created tests for pages license and contact in core app

--- core/tests/test_view.py
+++ core/tests/test_view.py
@@ -5,3 +5,13 @@ def test_home(client, db):
 	response = client.get('/')
 	assert response.status_code == 200
 	assertTemplateUsed(response, 'core/home.html')
+
+def test_contact(client):
+	response = client.get('/contact/')
+	assert response.status_code == 200
+	assertTemplateUsed(response, 'core/contact.html')
+
+def test_license(client):
+	response = client.get('/license/')
+	assert response.status_code == 200
+	assertTemplateUsed(response, 'core/license.html')
